32fcf31284ba84fe7ec39baacffdd1805fffc363
Ricardo Arana Reyes <ricardo.aranareyes@gmail.com>
2019-07-01 00:25:36 -0500

    Se agrega un ejemplo de android alarm, ya no hay compatibilidad con ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:saldocontrol/repository/database_creator.dart';
 import 'package:saldocontrol/screams/home.dart';
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 
-void main() async {
+void printMessage(String msg) => print('[${DateTime.now()}] $msg');
+
+void printPeriodic() => printMessage("Periodic!");
+void printOneShot() => printMessage("One shot!");
+
+Future<void> main() async {
+  final int periodicID = 0;
+  final int oneShotID = 1;
+
+  // Start the AlarmManager service.
+  await AndroidAlarmManager.initialize();
+
+  printMessage("main run");
   await DatabaseCreator().initDatabase();
   runApp(MyApp());
+
+  await AndroidAlarmManager.periodic(
+      const Duration(seconds: 5), periodicID, printPeriodic,
+      wakeup: true);
+  await AndroidAlarmManager.oneShot(
+      const Duration(seconds: 5), oneShotID, printOneShot);
+  
 }
+
 
 class MyApp extends StatelessWidget {
   @override
